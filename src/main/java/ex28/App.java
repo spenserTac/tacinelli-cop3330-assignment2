a7fa3461d3af4d764d3e96edf727ea3031bfcefd
Spenser <spenserdt@gmail.com>
2021-09-23 20:01:13 -0400

    finished ex28

---
+++ src/main/java/ex28/App.java
@@ -0,0 +1,19 @@
+package ex28;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int count = 0;
+        for(int i = 0; i < 5; i++){
+            System.out.print("Enter a number: ");
+            String temp_num = scanner.nextLine();
+            int num = Integer.parseInt(temp_num);
+            count += num;
+        }
+
+        System.out.printf("The total is %d.", count);
+    }
+}
